8c4286b6998b6f74226537241b65195a6400e6c1
sachingill <sachin.gill07@gmail.com>
2022-03-11 20:00:24 +0530

    Add ipynb file

--- last_eleement/last_element.py
+++ last_eleement/last_element.py
@@ -1,5 +1,12 @@
 # Find the last element of a list.
-# ex [1,2,3,4,5,6]
+# ex [1,2,3,4,5,6] the last element is 6
 
 ls = [1,2,3,4,5,6]
+
+
+def last_element(list):
+    return list[-1]
+
+print(last_element(ls))
+
 
